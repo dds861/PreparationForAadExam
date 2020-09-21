f6656c504ce4883ba8e9e794087f0c465bd7ab54
Daniyar <daniyar.nurgaliyev@babel.es>
2020-09-21 12:32:25 +0200

    5. Insert and get Data

--- app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
+++ app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.dd.preparationforaadexam
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val myDao = MyDatabase.getDatabase(applicationContext).myDao()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            val data = myDao.getAll()
+            if (data.isEmpty()) {
+                myDao.deleteAll()
+                myDao.insertMyDataClass(getList())
+            }
+        }
+    }
+
+    private fun getList(): List<MyDataClass> {
+
+        val arrayList = ArrayList<MyDataClass>()
+        arrayList.add(
+            MyDataClass(
+                tableId = 0,
+                name = "a",
+                description = "b"
+            )
+        )
+        return arrayList
     }
 }
