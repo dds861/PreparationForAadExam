7958f6eceaadaba5756e242aca13af6f2efc6313
Daniyar <daniyar.nurgaliyev@babel.es>
2020-09-22 10:34:57 +0200

    Simple Toast

--- app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
+++ app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.dd.preparationforaadexam
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showToast()
     }
+
+    private fun showToast() {
+        val toast = Toast.makeText(this, "This is a toast", Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.CENTER, 0, 0)
+        toast.show()
+    }
+
+
 }
