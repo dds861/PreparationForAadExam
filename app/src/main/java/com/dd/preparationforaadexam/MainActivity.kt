f4084e49a84852f6dca7878eebea107dc71fe3f9
Daniyar <daniyar.nurgaliyev@babel.es>
2020-09-22 10:35:45 +0200

    Custom Toast

--- app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
+++ app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
@@ -2,6 +2,9 @@ package com.dd.preparationforaadexam
 
 import android.os.Bundle
 import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        showToast()
+        showCustomToast()
     }
 
     private fun showToast() {
@@ -19,5 +22,20 @@ class MainActivity : AppCompatActivity() {
         toast.show()
     }
 
+
+    private fun showCustomToast(){
+        // Get the custom layout and inflate it
+        val inflater = LayoutInflater.from(this)
+        val layout = inflater.inflate(R.layout.custom_toast_layout, findViewById(R.id.customToastLayout))
+
+        // Build a toast message that uses the custom layout
+        val tv = layout.findViewById<View>(R.id.textContent) as TextView
+        tv.text = "This is a custom toast"
+        val toast = Toast(applicationContext)
+        toast.duration = Toast.LENGTH_SHORT
+        toast.setGravity(Gravity.BOTTOM or Gravity.START, 100, 100)
+        toast.view = layout
+        toast.show()
+    }
 
 }
