5feb941c753198379bbdd186dc768b9fedc34f93
Daniyar <daniyar.nurgaliyev@babel.es>
2020-09-23 10:50:16 +0200

    Dialog button click listener callbacks

--- app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
+++ app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
@@ -3,34 +3,45 @@ package com.dd.preparationforaadexam
 import android.app.AlertDialog
 import android.app.DatePickerDialog
 import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.DialogFragment
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SimpleDialogFragment.SimpleDialogListener {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        val simpleDialog = SimpleDialogFragment()
-//        simpleDialog.show(supportFragmentManager, "SimpleDialogFragment")
+        val simpleDialog = SimpleDialogFragment()
+        simpleDialog.show(supportFragmentManager, "SimpleDialogFragment")
 
 //        val complexDialog = SingleChoiceDialogFragment()
 //        complexDialog.show(supportFragmentManager, "SingleChoiceDialogFragment")
 
-        showDatePickerDialog()
+//        showDatePickerDialog()
     }
 
-    private fun showDatePickerDialog(){
+    private fun showDatePickerDialog() {
         // Get a calendar instance
         val cal = Calendar.getInstance()
 
         // Create a DatePickerDialog
-        val datePicker = DatePickerDialog(this,
+        val datePicker = DatePickerDialog(
+            this,
             DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
-                Log.i("autolog", String.format("Date Chosen -- day: %d, month: %d, year: %d", dayOfMonth, monthOfYear, year))
+                Log.i(
+                    "autolog",
+                    String.format(
+                        "Date Chosen -- day: %d, month: %d, year: %d",
+                        dayOfMonth,
+                        monthOfYear,
+                        year
+                    )
+                )
             },
             cal.get(Calendar.YEAR),
             cal.get(Calendar.MONTH),
@@ -42,19 +53,6 @@ class MainActivity : AppCompatActivity() {
         datePicker.show()
     }
 
-    class SimpleDialogFragment : DialogFragment() {
-
-        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-            val builder = AlertDialog.Builder(activity!!)
-
-            builder.setTitle("My Title")
-            builder.setMessage("My message")
-            builder.setPositiveButton("Yes") { dialog, which -> }
-            builder.setNegativeButton("No") { dialog, which -> }
-            builder.setNeutralButton("Neutral") { dialog, which -> }
-            return builder.create()
-        }
-    }
 
     class SingleChoiceDialogFragment : DialogFragment() {
         private val colors = arrayOf("Red", "Blue", "Green", "Yellow")
@@ -63,8 +61,62 @@ class MainActivity : AppCompatActivity() {
             val builder = AlertDialog.Builder(activity)
             builder.setTitle("My title")
 
-            builder.setItems(colors) { dialog, which ->Log.i("autolog", "colors: " + colors[which])}
+            builder.setItems(colors) { dialog, which ->
+                Log.i(
+                    "autolog",
+                    "colors: " + colors[which]
+                )
+            }
             return builder.create()
         }
+    }
+
+    override fun onPositiveResult(dlg: DialogFragment) {
+        Log.i("autolog", "Dialog Positive Result")
+    }
+
+    override fun onNegativeResult(dlg: DialogFragment) {
+        Log.i("autolog", "Dialog Negative Result")
+    }
+
+    override fun onNeutralResult(dlg: DialogFragment) {
+        Log.i("autolog", "Dialog Neutral Result")
+    }
+
+
+}
+
+class SimpleDialogFragment : DialogFragment() {
+
+    private var mHost: SimpleDialogListener? = null
+
+    interface SimpleDialogListener {
+        fun onPositiveResult(dlg: DialogFragment)
+        fun onNegativeResult(dlg: DialogFragment)
+        fun onNeutralResult(dlg: DialogFragment)
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = AlertDialog.Builder(activity!!)
+
+        builder.setTitle("My Title")
+        builder.setMessage("My message")
+        builder.setPositiveButton("Yes") { dialog, which ->
+            mHost!!.onPositiveResult(this@SimpleDialogFragment)
+        }
+        builder.setNegativeButton("No") { dialog, which ->
+            mHost!!.onNegativeResult(this@SimpleDialogFragment)
+        }
+        builder.setNeutralButton("Neutral") { dialog, which ->
+            mHost!!.onNeutralResult(this@SimpleDialogFragment)
+        }
+
+
+        return builder.create()
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mHost = context as SimpleDialogListener
     }
 }
