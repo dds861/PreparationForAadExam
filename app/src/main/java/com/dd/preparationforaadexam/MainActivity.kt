1cd4427692212abaa8dad2d7609e3af6f0d9817c
Daniyar <daniyar.nurgaliyev@babel.es>
2020-09-23 13:34:36 +0200

    Custom Dialog

--- app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
+++ app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
@@ -16,6 +16,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val customDialog = CustomDialogFragment()
+        customDialog.show(supportFragmentManager, "CustomDialogFragment")
     }
 }
 
+class CustomDialogFragment : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = androidx.appcompat.app.AlertDialog.Builder(activity!!)
+
+        // Create the custom layout using the LayoutInflater class
+        val inflater = activity!!.layoutInflater
+        val v = inflater.inflate(R.layout.custom_dialog_layout, null)
+
+        // Build the dialog
+        builder.setTitle("Please enter your name")
+            .setPositiveButton("OK") { dialog, which -> Log.i("autolog", "OK Clicked") }
+            .setNegativeButton("Cancel") { dialog, which -> Log.i("autolog", "Cancel clicked") }
+            .setView(v)
+
+        return builder.create()
+    }
+}
