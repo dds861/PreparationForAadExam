8498982860e3541b686a439a1f978c8c3e3db4ff
Daniyar <daniyar.nurgaliyev@babel.es>
2020-09-23 13:55:51 +0200

    add listeners

--- app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
+++ app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
@@ -1,16 +1,14 @@
 package com.dd.preparationforaadexam
 
-import android.app.AlertDialog
-import android.app.DatePickerDialog
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.DialogFragment
-import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CustomDialogFragment.CustomDialogListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,23 +17,54 @@ class MainActivity : AppCompatActivity() {
         val customDialog = CustomDialogFragment()
         customDialog.show(supportFragmentManager, "CustomDialogFragment")
     }
+
+    override fun onPositiveResult(dlg: DialogFragment, text: String) {
+        Log.i("autolog", "onPositiveResult:$text");
+    }
+
+    override fun onNegativeResult(dlg: DialogFragment) {
+        Log.i("autolog", "onNegativeResult:");
+    }
 }
 
 class CustomDialogFragment : DialogFragment() {
+    private var mHost: CustomDialogListener? = null
+
+    interface CustomDialogListener {
+        fun onPositiveResult(dlg: DialogFragment, text: String)
+        fun onNegativeResult(dlg: DialogFragment)
+    }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+
         val builder = androidx.appcompat.app.AlertDialog.Builder(activity!!)
 
         // Create the custom layout using the LayoutInflater class
         val inflater = activity!!.layoutInflater
         val v = inflater.inflate(R.layout.custom_dialog_layout, null)
 
+        val firstName = v.findViewById<EditText>(R.id.edtFirstName)
+        val lastName = v.findViewById<EditText>(R.id.edtLastName)
+
         // Build the dialog
         builder.setTitle("Please enter your name")
-            .setPositiveButton("OK") { dialog, which -> Log.i("autolog", "OK Clicked") }
-            .setNegativeButton("Cancel") { dialog, which -> Log.i("autolog", "Cancel clicked") }
+            .setPositiveButton("OK") { dialog, which ->
+                Log.i("autolog", "OK Clicked")
+                mHost!!.onPositiveResult(this@CustomDialogFragment, "${firstName.text} ${lastName.text}")
+            }
+            .setNegativeButton("Cancel") { dialog, which ->
+                Log.i("autolog", "Cancel clicked")
+                mHost!!.onNegativeResult(this@CustomDialogFragment)
+            }
             .setView(v)
 
         return builder.create()
+    }
+
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mHost = context as CustomDialogListener
     }
 }
