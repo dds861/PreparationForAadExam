1758a6fe44cb972e4b009d58105177dcd85c58e4
Daniyar <dds861@gmail.com>
2020-10-06 09:46:48 +0200

    Clean Up Project

--- app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
+++ app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
@@ -1,15 +1,7 @@
 package com.dd.preparationforaadexam
 
-import android.app.AlarmManager
-import android.app.PendingIntent
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
-import org.greenrobot.eventbus.EventBus
-import org.greenrobot.eventbus.Subscribe
-import org.greenrobot.eventbus.ThreadMode
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,35 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //1. Register EventBus
-        EventBus.getDefault().register(this)
 
-        btnStart.setOnClickListener {
-            //4. Create Intent
-            val intent = Intent(this, MyReceiver::class.java)
-            intent.putExtra("payload", "From the original intent")
-
-            //5. Create PendingIntent
-            val pendingIntent = PendingIntent.getBroadcast(
-                applicationContext, 1, intent, 0
-            )
-
-            //6. Create AlarmManager with 3 seconds delay
-            val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
-            alarmManager[AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 3000] = pendingIntent
-            Log.i("autolog", "Alarm set in 3 seconds: ")
-        }
-    }
-
-    override fun onDestroy() {
-        //2. Unregister EventBus
-        EventBus.getDefault().unregister(this)
-        super.onDestroy()
-    }
-
-    //3. Receive Event from EventBus
-    @Subscribe(threadMode = ThreadMode.MAIN)
-    fun onMessageEvent(event: MessageEvent) {
-        Log.i("autolog", "event.message: " + event.message);
     }
 }
--- app/src/main/java/com/dd/preparationforaadexam/MessageEvent.kt
+++
@@ -1,3 +0,0 @@
-package com.dd.preparationforaadexam
-
-data class MessageEvent(var message: String)
