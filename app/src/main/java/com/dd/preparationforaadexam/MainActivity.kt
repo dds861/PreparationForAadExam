f64b3ae26b3ebe1823ef2bf0c5b6868fdbed0c27
Daniyar <dds861@gmail.com>
2020-10-06 09:47:05 +0200

    Popup menu added

--- app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
+++ app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.dd.preparationforaadexam
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,5 +13,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        btnStart.setOnClickListener { view ->
+            val popupMenu = PopupMenu(this, view)
+            popupMenu.menuInflater.inflate(R.menu.more_menu, popupMenu.menu)
+
+            popupMenu.setOnMenuItemClickListener {
+                when (it.itemId) {
+                    R.id.menu1 -> Toast.makeText(this, "menu1", Toast.LENGTH_SHORT).show()
+                    R.id.menu2 -> Toast.makeText(this, "menu2", Toast.LENGTH_SHORT).show()
+                    else -> Toast.makeText(this, "nothing", Toast.LENGTH_SHORT).show()
+                }
+                return@setOnMenuItemClickListener false
+            }
+
+            popupMenu.show()
+        }
     }
 }
