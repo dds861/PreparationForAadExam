a6bb218c656d8e20a2de920c50875afeaba6ce2e
Daniyar <dds861@gmail.com>
2020-10-07 10:51:09 +0200

    CleanUp project

--- app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
+++ app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.dd.preparationforaadexam
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.PopupMenu
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,21 +12,5 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        btnStart.setOnClickListener { view ->
-            val popupMenu = PopupMenu(this, view)
-            popupMenu.menuInflater.inflate(R.menu.more_menu, popupMenu.menu)
-
-            popupMenu.setOnMenuItemClickListener {
-                when (it.itemId) {
-                    R.id.menu1 -> Toast.makeText(this, "menu1", Toast.LENGTH_SHORT).show()
-                    R.id.menu2 -> Toast.makeText(this, "menu2", Toast.LENGTH_SHORT).show()
-                    else -> Toast.makeText(this, "nothing", Toast.LENGTH_SHORT).show()
-                }
-                return@setOnMenuItemClickListener false
-            }
-
-            popupMenu.show()
-        }
     }
 }
