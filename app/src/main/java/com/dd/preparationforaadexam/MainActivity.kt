17d0a59539dc37b9704b64c3997bb55bee6c0f54
Daniyar <dds861@gmail.com>
2020-10-07 10:51:35 +0200

    Read text files from Assets

--- app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
+++ app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
@@ -12,5 +12,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //set text to TextView ny passing *.txt name
+        tvText.text = readFileFromAssets("text_file.txt")
+    }
+
+    private fun readFileFromAssets(filename: String): String {
+
+        //We will build the string line by line from *.txt file
+        val builder = StringBuilder()
+
+        //BufferedReader is needed to read the *.txt file
+        //Create and Initialize BufferedReader
+        val reader = BufferedReader(InputStreamReader(assets.open(filename)))
+
+        //This variable will contain the text
+        var line: String?
+
+        //check if there is a more line available
+        while (reader.readLine().also { line = it } != null) {
+            builder.append(line).append("\n")
+        }
+
+        //Need to close the BufferedReader
+        reader.close()
+
+        //just return the String of the *.txt file
+        return builder.toString()
     }
 }
