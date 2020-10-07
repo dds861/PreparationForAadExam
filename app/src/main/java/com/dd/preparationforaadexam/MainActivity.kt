bc1095f31a2c4849b88c3c85c5b00cc7cdf75607
Daniyar <dds861@gmail.com>
2020-10-07 16:00:54 +0200

    add sensor implementation

--- app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
+++ app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
@@ -1,13 +1,42 @@
 package com.dd.preparationforaadexam
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+
+//implement SensorEventListener
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    //Create 2 objects: SensorManager and Sensor
+    private lateinit var sensorManager: SensorManager
+    private lateinit var accelerometer: Sensor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Initialize SensorManager and Sensor
+        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
     }
+
+    //register SensorManager
+    override fun onResume() {
+        super.onResume()
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    //unregister SensorManager
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
+    }
+
+    //override 2 functions
+    override fun onSensorChanged(event: SensorEvent) {}
+    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {}
 }
