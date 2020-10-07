42d2073d3b31e7450ba7dca63f5ed5efd8215dd8
Daniyar <dds861@gmail.com>
2020-10-07 17:21:10 +0200

    Text to Speech

--- app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
+++ app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
@@ -1,15 +1,34 @@
 package com.dd.preparationforaadexam
 
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 
-class MainActivity : AppCompatActivity() {
+//Implement TextToSpeech.OnInitListener
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Create and initialize TextToSpeech object
+        val tts = TextToSpeech(applicationContext, this)
 
+        //do action on button click
+        btnClick.setOnClickListener {
+
+            //set Language type
+            tts.language = Locale.ENGLISH
+
+            //Run speak(...) function with required parameters
+            tts.speak("hello, this is text to speech", TextToSpeech.QUEUE_FLUSH, null, "speech")
+        }
+    }
+
+    //override onInit function
+    override fun onInit(status: Int) {
     }
 }
