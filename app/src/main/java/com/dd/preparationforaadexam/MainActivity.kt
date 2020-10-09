992db5271f7cb4047423a13df3c102aef5417c01
Daniyar <dds861@gmail.com>
2020-10-09 10:19:44 +0200

    Simple Dispatchers.Main example

--- app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
+++ app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.dd.preparationforaadexam
 
 import android.os.Bundle
-import android.speech.tts.TextToSpeech
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +15,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        CoroutineScope(Dispatchers.Main).launch {
+            tvText.text = getText()
+        }
+    }
+
+    private suspend fun getText(): String {
+        delay(1000)
+        return "some text"
     }
 }
