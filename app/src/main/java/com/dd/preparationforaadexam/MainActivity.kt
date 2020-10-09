ea863bb855b78d1e5a56596b02a61211ec78d0e4
Daniyar <dds861@gmail.com>
2020-10-09 20:20:07 +0200

    Switch between foreground and background threads

--- app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
+++ app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
@@ -2,11 +2,10 @@ package com.dd.preparationforaadexam
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 
 class MainActivity : AppCompatActivity() {
@@ -16,12 +15,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         CoroutineScope(Dispatchers.Main).launch {
-            tvText.text = getText()
+            val s = getText()
         }
     }
 
     private suspend fun getText(): String {
-        delay(1000)
-        return "some text"
+        return withContext(Dispatchers.IO) {
+            return@withContext "Text from IO"
+        }
     }
 }
