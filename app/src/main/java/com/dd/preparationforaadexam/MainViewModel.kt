db493c2ca52a2314d46223cf9257d66f79deb308
Daniyar <dds861@gmail.com>
2020-10-15 12:04:24 +0200

    Cancel Coroutine with a Job Reference

--- app/src/main/java/com/dd/preparationforaadexam/MainViewModel.kt
+++ app/src/main/java/com/dd/preparationforaadexam/MainViewModel.kt
@@ -4,20 +4,23 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import java.net.URL
-import java.nio.charset.Charset
 
 //implement ViewModel object
-class MainViewModel: ViewModel() {
+class MainViewModel : ViewModel() {
 
     //Create MutableLiveData object
     val myData = MutableLiveData<String>()
 
+    //Create Job object
+    private lateinit var job: Job
+
     //Set value to myData variable inside viewModelScope
     fun doWork() {
-        viewModelScope.launch {
+        //viewModelScope.launch wil return Job object
+        job = viewModelScope.launch {
             myData.value = getText()
         }
     }
@@ -26,6 +29,15 @@ class MainViewModel: ViewModel() {
     private suspend fun getText(): String {
         return withContext(Dispatchers.IO) {
             return@withContext "Text from IO"
+        }
+    }
+
+    //Function that Cancels the Job
+    fun cancelJob() {
+        try {
+            //Here we can cancel The Job
+            job.cancel()
+        } catch (ignore: UninitializedPropertyAccessException) {
         }
     }
 }
