17755ed2287b262ab0c73578f50c0a5fe162f0e2
Daniyar <dds861@gmail.com>
2020-10-16 17:47:28 +0200

    CleanUpProject

--- app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
+++ app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
@@ -1,9 +1,6 @@
 package com.dd.preparationforaadexam
 
-import android.app.Activity
 import android.os.Bundle
-import android.os.Handler
-import android.os.ResultReceiver
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,26 +11,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnClick.setOnClickListener {
-            //5. Create inner class MyResultReceiver(...) object
-            val receiver = MyResultReceiver(Handler())
-
-            //6. And pass that receiver to the static function
-            MyIntentService.startActionFoo(this, "param1", receiver)
-        }
-    }
-
-    //1. Create class with ResultReceiver implementation
-    inner class MyResultReceiver(handler: Handler) : ResultReceiver(handler) {
-
-        //2. override the onReceiveResult function
-        override fun onReceiveResult(resultCode: Int, resultData: Bundle?) {
-
-            //3. check for resultCode
-            if (resultCode == Activity.RESULT_OK) {
-
-                //4. Set String to TextView, which is received by key from Bundle
-                tvText.text = resultData?.getString(FILE_CONTENTS_KEY)
-            }
         }
     }
 }
