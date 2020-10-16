40f673f1a83332594393c931b249c1f16ba2af22
Daniyar <dds861@gmail.com>
2020-10-16 10:31:00 +0200

    migrate from IntentService to JobIntentService.

--- app/src/main/java/com/dd/preparationforaadexam/MyIntentService.kt
+++ app/src/main/java/com/dd/preparationforaadexam/MyIntentService.kt
@@ -4,42 +4,35 @@ import android.app.IntentService
 import android.content.Context
 import android.content.Intent
 import android.util.Log
+import androidx.core.app.JobIntentService
 
 private const val ACTION_FOO = "com.dd.preparationforaadexam.action.FOO"
 private const val EXTRA_PARAM1 = "com.dd.preparationforaadexam.extra.PARAM1"
 
-//0. Implement IntentService
-class MyIntentService : IntentService("MyIntentService") {
+//1. Create constant
+const val JOB_ID = 1001
 
-    override fun onHandleIntent(intent: Intent?) {
+//0. Implement JobIntentService
+class MyIntentService : JobIntentService() {
 
-        //6. Find Action
-        when (intent?.action) {
+    //2. Change from onHandleIntent(...) to onHandleWork(...)
+    override fun onHandleWork(intent: Intent) {
+        when (intent.action) {
             ACTION_FOO -> {
-                //7. get String from intent by key
                 Log.i("autolog", intent.getStringExtra(EXTRA_PARAM1))
             }
         }
     }
 
     companion object {
-
-        //1. Create static functions that will start the Service
         @JvmStatic
         fun startActionFoo(context: Context, param1: String) {
-
-            //2. Create Intent object
             val intent = Intent(context, MyIntentService::class.java).apply {
-
-                //3. set action with key
                 action = ACTION_FOO
-
-                //4. put extra data
                 putExtra(EXTRA_PARAM1, param1)
             }
-
-            //5. Start the service
-            context.startService(intent)
+            //4. Instead of context.startService(...) -> put enqueueWork(...)
+            enqueueWork(context, MyIntentService::class.java, JOB_ID, intent)
         }
     }
 }
