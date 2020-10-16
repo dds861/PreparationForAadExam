29c86487bb1e338fd89ce05ad03d3b9dbad84d1d
Daniyar <dds861@gmail.com>
2020-10-16 18:10:23 +0200

    Set work request constraints

--- app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
+++ app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
@@ -2,6 +2,8 @@ package com.dd.preparationforaadexam
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.work.Constraints
+import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import kotlinx.android.synthetic.main.activity_main.*
@@ -13,7 +15,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btnClick.setOnClickListener {
-            val workRequest = OneTimeWorkRequestBuilder<MyWorker>().build()
+
+            //The application will wait when the event of Network Connection will be available.
+            //Upon the satisfaction of this condition WorkManager will continue the work
+            val constraints = Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED)
+                .build()
+
+            //Add "constraints"
+            val workRequest = OneTimeWorkRequestBuilder<MyWorker>()
+                .setConstraints(constraints)
+                .build()
             WorkManager.getInstance(applicationContext).enqueue(workRequest)
         }
     }
