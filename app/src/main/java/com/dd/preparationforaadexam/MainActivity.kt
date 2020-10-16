2190d6ce0c7c1cb614f8fa1d6d724296fea13bf4
Daniyar <dds861@gmail.com>
2020-10-16 11:06:13 +0200

    Send results from JobIntentService to the UI

--- app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
+++ app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
@@ -1,19 +1,39 @@
 package com.dd.preparationforaadexam
 
+import android.app.Activity
 import android.os.Bundle
+import android.os.Handler
+import android.os.ResultReceiver
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnClick.setOnClickListener {
-            //Call static function to launch Service by passing context and String value
-            MyIntentService.startActionFoo(this, "param1")
+            //5. Create inner class MyResultReceiver(...) object
+            val receiver = MyResultReceiver(Handler())
+
+            //6. And pass that receiver to the static function
+            MyIntentService.startActionFoo(this, "param1", receiver)
+        }
+    }
+
+    //1. Create class with ResultReceiver implementation
+    inner class MyResultReceiver(handler: Handler) : ResultReceiver(handler) {
+
+        //2. override the onReceiveResult function
+        override fun onReceiveResult(resultCode: Int, resultData: Bundle?) {
+
+            //3. check for resultCode
+            if (resultCode == Activity.RESULT_OK) {
+
+                //4. Set String to TextView, which is received by key from Bundle
+                tvText.text = resultData?.getString(FILE_CONTENTS_KEY)
+            }
         }
     }
 }
--- app/src/main/java/com/dd/preparationforaadexam/MyIntentService.kt
+++ app/src/main/java/com/dd/preparationforaadexam/MyIntentService.kt
@@ -1,37 +1,51 @@
 package com.dd.preparationforaadexam
 
-import android.app.IntentService
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import android.os.ResultReceiver
 import android.util.Log
 import androidx.core.app.JobIntentService
 
 private const val ACTION_FOO = "com.dd.preparationforaadexam.action.FOO"
 private const val EXTRA_PARAM1 = "com.dd.preparationforaadexam.extra.PARAM1"
-
-//1. Create constant
 const val JOB_ID = 1001
 
-//0. Implement JobIntentService
-class MyIntentService : JobIntentService() {
+//0. Create constant
+const val FILE_CONTENTS_KEY = "file_contents_key"
+const val RECEIVER_KEY = "receiver_key"
 
-    //2. Change from onHandleIntent(...) to onHandleWork(...)
+class MyIntentService : JobIntentService() {
     override fun onHandleWork(intent: Intent) {
         when (intent.action) {
             ACTION_FOO -> {
-                Log.i("autolog", intent.getStringExtra(EXTRA_PARAM1))
+
+                //3. get the content from intent
+                val content = intent.getStringExtra(EXTRA_PARAM1)
+
+                //4. Create Bundle, put data inside
+                val bundle = Bundle()
+                bundle.putString(FILE_CONTENTS_KEY, content)
+
+                //5. Create ResultReceiver with key, and send bundle back to receiver
+                val receiver = intent.getParcelableExtra<ResultReceiver>(RECEIVER_KEY)
+                receiver?.send(Activity.RESULT_OK, bundle)
             }
         }
     }
 
     companion object {
         @JvmStatic
-        fun startActionFoo(context: Context, param1: String) {
+        //1. Add 3rd parameter to the function
+        fun startActionFoo(context: Context, param1: String, receiver: MainActivity.MyResultReceiver) {
             val intent = Intent(context, MyIntentService::class.java).apply {
                 action = ACTION_FOO
                 putExtra(EXTRA_PARAM1, param1)
+
+                //2. add "receiver" to Extra
+               putExtra(RECEIVER_KEY, receiver)
             }
-            //4. Instead of context.startService(...) -> put enqueueWork(...)
             enqueueWork(context, MyIntentService::class.java, JOB_ID, intent)
         }
     }
