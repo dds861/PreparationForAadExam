e00cbc49d88e62a926c6924a5fcba533b414b7db
Daniyar <dds861@gmail.com>
2020-10-19 11:56:15 +0200

    Project Cleanup

--- app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
+++ app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
@@ -2,10 +2,6 @@ package com.dd.preparationforaadexam
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
-import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.WorkInfo
-import androidx.work.WorkManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -14,25 +10,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-btnClick.setOnClickListener {
+        btnClick.setOnClickListener {
 
-    val workRequest = OneTimeWorkRequestBuilder<MyWorker>().build()
-    val workManager = WorkManager.getInstance(applicationContext)
-    workManager.enqueue(workRequest)
-
-    workManager.getWorkInfoByIdLiveData(workRequest.id).observe(this, Observer {
-        tvText.text = when (it.state) {
-            WorkInfo.State.SUCCEEDED -> {
-                it.outputData.getString(DATA_KEY)
-            }
-
-            //1. Add State item and set it's value to TextView
-            WorkInfo.State.RUNNING -> {
-                it.progress.getString(MESSAGE_KEY)
-            }
-            else -> "Empty"
         }
-    })
-}
     }
 }
