1a99ce361862508969494787c40774d2438d5d9a
Daniyar <dds861@gmail.com>
2020-10-19 16:38:51 +0200

    Run service in a foreground

--- app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
+++ app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
@@ -6,7 +6,6 @@ import android.content.Intent
 import android.content.ServiceConnection
 import android.os.Bundle
 import android.os.IBinder
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -44,4 +43,5 @@ class MainActivity : AppCompatActivity() {
         super.onStop()
         unbindService(connection)
     }
+
 }
--- app/src/main/java/com/dd/preparationforaadexam/MyService.kt
+++ app/src/main/java/com/dd/preparationforaadexam/MyService.kt
@@ -1,17 +1,21 @@
 package com.dd.preparationforaadexam
 
-import android.app.Service
+import android.app.*
 import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
 import android.media.RingtoneManager
 import android.net.Uri
 import android.os.Binder
+import android.os.Build
 import android.os.IBinder
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 
 class MyService : Service() {
 
     private val binder = MyServiceBinder()
+    private lateinit var player: MediaPlayer
 
     override fun onBind(intent: Intent): IBinder {
         return binder
@@ -21,14 +25,8 @@ class MyService : Service() {
         return START_STICKY
     }
 
-    inner class MyServiceBinder : Binder() {
-        fun getService() = this@MyService
-    }
+    inner class MyServiceBinder : Binder() {fun getService() = this@MyService}
 
-    //1. Create MediaPlayer object
-    private lateinit var player: MediaPlayer
-
-    //2. Create custom function that will Play app's ringtone
     fun startAudio(context: Context) {
         val soundUri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE)
         player = MediaPlayer().also {
@@ -37,10 +35,61 @@ class MyService : Service() {
             it.start();
         }
 
+        //3. Show the notification
+        displayForegroundNotification()
     }
 
-    //3. Create custom function that will Stop app's ringtone
     fun stopAudio() {
         player.stop()
+
+        //4. Stop the notification
+        stopForeground(false)
+    }
+
+    //2. Display Notification
+    private fun displayForegroundNotification() {
+
+        //2.1. Check the platform version
+        val channelId = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel()
+        } else {
+            ""
+        }
+
+        //2.2. Create PendingIntent
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val pendingIntent =
+            PendingIntent.getActivity(this, 0, notificationIntent, 0)
+
+        //2.3. Create Notification
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setContentTitle("Playing music")
+            .setContentText("any Text")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setWhen(0)
+            .build()
+
+        //2.4. Start the notification
+        startForeground(1001, notification)
+
+    }
+
+    //1. Create Notification channel
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(): String {
+        val channelId = "my_service"
+        val channelName = "Music Service"
+
+        //1.2. Create NotificationChannel
+        val notificationChannel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_NONE)
+        notificationChannel.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+
+        //1.3. Create NotificationManager
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(notificationChannel)
+
+        return channelId
     }
 }
