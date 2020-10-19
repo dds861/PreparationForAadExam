5943ae043403b3e772952efb01da24e98df36ec8
Daniyar <dds861@gmail.com>
2020-10-19 14:48:39 +0200

    Play audio in a service with MediaPlayer

--- app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
+++ app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
@@ -13,37 +13,30 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    //1. Create  instance of MyService
     private lateinit var myService: MyService
 
     private val connection = object : ServiceConnection {
-        override fun onServiceDisconnected(name: ComponentName?) {
-        }
+        override fun onServiceDisconnected(name: ComponentName?) {}
 
         override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-            Log.i("autolog", "Service connecting")
             val binder = service as MyService.MyServiceBinder
             myService = binder.getService()
         }
-
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnClick.setOnClickListener {
-            myService.startAction()
-        }
+        //1. Start and Stop audio
+        btnStart.setOnClickListener { myService.startAudio(this) }
+        btnStop.setOnClickListener { myService.stopAudio() }
     }
-
 
     override fun onStart() {
         super.onStart()
         val intent = Intent(this, MyService::class.java)
         bindService(intent, connection, Context.BIND_AUTO_CREATE)
-
-        //1. Start the service to run in the background indefinitely.
         startService(intent)
     }
 
--- app/src/main/java/com/dd/preparationforaadexam/MyService.kt
+++ app/src/main/java/com/dd/preparationforaadexam/MyService.kt
@@ -1,10 +1,13 @@
 package com.dd.preparationforaadexam
 
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.media.MediaPlayer
+import android.media.RingtoneManager
+import android.net.Uri
 import android.os.Binder
 import android.os.IBinder
-import android.util.Log
 
 class MyService : Service() {
 
@@ -14,16 +17,30 @@ class MyService : Service() {
         return binder
     }
 
-    //The system invokes this method by calling startService() in MainActivity(...)
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         return START_STICKY
     }
 
-    fun startAction() {
-        Log.i("autolog", "Starting Action ")
-    }
-
     inner class MyServiceBinder : Binder() {
         fun getService() = this@MyService
+    }
+
+    //1. Create MediaPlayer object
+    private lateinit var player: MediaPlayer
+
+    //2. Create custom function that will Play app's ringtone
+    fun startAudio(context: Context) {
+        val soundUri: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE)
+        player = MediaPlayer().also {
+            it.setDataSource(context, soundUri)
+            it.prepare();
+            it.start();
+        }
+
+    }
+
+    //3. Create custom function that will Stop app's ringtone
+    fun stopAudio() {
+        player.stop()
     }
 }
