efb863ecae66b56616ef0c65c981103c96e64e97
Daniyar <dds861@gmail.com>
2020-10-27 13:38:14 +0100

    CleanUp

--- app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
+++ app/src/main/java/com/dd/preparationforaadexam/MainActivity.kt
@@ -12,35 +12,9 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var myService: MyService
-
-    private val connection = object : ServiceConnection {
-        override fun onServiceDisconnected(name: ComponentName?) {}
-
-        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-            val binder = service as MyService.MyServiceBinder
-            myService = binder.getService()
-        }
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnStart.setOnClickListener { myService.startAudio(this) }
-        btnStop.setOnClickListener { myService.stopAudio() }
     }
-
-    override fun onStart() {
-        super.onStart()
-        val intent = Intent(this, MyService::class.java)
-        bindService(intent, connection, Context.BIND_AUTO_CREATE)
-        startService(intent)
-    }
-
-    override fun onStop() {
-        super.onStop()
-        unbindService(connection)
-    }
-
 }
